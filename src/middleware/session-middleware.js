888d23f58b51dba129361dfd6deb0ed6db3dde12
hoangvl <hoangvlbk61@gmail.com>
2022-02-01 16:57:58 +0700

    Update session middleware bypoass mechanism

--- src/middleware/session-middleware.js
+++ src/middleware/session-middleware.js
@@ -1,24 +1,40 @@
-const Session = require('../persistence/sessions');
+const Session = require("../persistence/sessions");
+
+const ByPassUrl = [
+  { path: "/verify", method: "post" },
+  { path: "", method: "get" },
+  { path: "/api/sessions", method: "post" },
+  { path: "/api/users", method: "post" },
+];
 
 const sessionMiddleware = async (request, response, next) => {
-  if (!request.session.id) {
+  const ssid = request.session.id || request.headers.ssid;
+  if (!ssid) {
+    // Bypass default path
+    const path = request.path;
+    const method = request.method;
+    if (
+      ByPassUrl.some(
+        (bpu) => bpu.path === path && bpu.method === method.toLowerCase()
+      )
+    ) {
+      return next();
+    }
     return response.sendStatus(401);
   }
 
   try {
     // eslint-disable-next-line unicorn/no-fn-reference-in-iterator
-    const session = await Session.find(request.session.id);
+    const session = await Session.find(ssid);
     if (!session) {
-      request.session.id = null;
+      ssid = null;
       return response.sendStatus(401);
     }
 
     request.userId = session.userId;
     next();
   } catch (error) {
-    console.error(
-      `SessionMiddleware(${request.session.id}) >> Error: ${error.stack}`
-    );
+    console.error(`SessionMiddleware(${ssid}) >> Error: ${error.stack}`);
     return response.sendStatus(500);
   }
 };
