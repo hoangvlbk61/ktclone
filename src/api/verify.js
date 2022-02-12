96d6daebe3ae75043e95d33385e50d60a1d9cfcd
hoangvl <hoangvlbk61@gmail.com>
2022-02-12 16:13:49 +0700

    Update verify

--- src/api/verify.js
+++ src/api/verify.js
@@ -2,20 +2,23 @@ const {Router} = require('express');
 const bcrypt = require('bcrypt');
 
 const User = require('../persistence/users');
+const Session = require('../persistence/sessions');
 
 const router = new Router();
 
 router.post('/', async (request, response) => {
   try {
-    const {email, password} = request.body;
-    const user = await User.find(email);
-    if (!user || !(await bcrypt.compare(password, user.password))) {
-      return response.status(403).json({});
-    }
-    response.status(200).json();
+    let ssid = request.headers.ssid || request.session.id; 
+    const session = await Session.find(ssid);
+    if(!session)  
+    return response.status(403).json({});
+    const userId = session.userId;
+    const user = await User.findById(userId);
+    if(user.password) delete user.password;
+    response.status(200).json(user);
   } catch (error) {
     console.error(
-      `POST verify ({ email: ${request.body.email} }) >> ${error.stack})`
+      `POST verify ({  }) >> ${error.stack})`
     );
     response.status(500).json();
   }
