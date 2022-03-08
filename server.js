51f7c949164f786a875029aa99a6189dee915f3b
hoangvl <hoangvlbk61@gmail.com>
2022-03-08 23:11:09 +0700

    Move to app

--- server.js
+++ server.js
@@ -31,7 +31,19 @@ app.use(
   })
 );
 app.use(helmet());
-
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+})
 app.use(api);
 
 var httpServer = http.createServer(app);
--- src/api/index.js
+++ src/api/index.js
@@ -11,28 +11,8 @@ const taskUser = require("./task-user");
 const verify = require("./verify");
 const withdraw = require("./withdraw");
 const file = require("./file");
-const headerCors = function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-}
+
 router.use(sessionMiddleware);
-router.use(headerCors);
-user.use(headerCors);
-session.use(headerCors);
-task.use(headerCors);
-taskUser.use(headerCors);
-verify.use(headerCors);
-withdraw.use(headerCors);
-file.use(headerCors);
 
 router.use("/api/users", user);
 router.use("/api/task-user", taskUser);
@@ -41,7 +21,5 @@ router.use("/api/tasks", task);
 router.use("/api/withdraw", withdraw);
 router.use("/verify", verify);
 router.use("/api/files", file);
-
-router.use();
 
 module.exports = router;
